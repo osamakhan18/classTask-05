7aba25fdd4f517018da14f55d11b5677544421c9
osamakhan18 <badishakhan18@gmail.com>
2024-09-29 00:50:54 -0700

    set the button in javascript

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-var dob = document.getElementById('input-dob')
+
+
+
+var btn = document.getElementById('btn')
+
+// btn.addEventListener('click',function(){
+
+//     var dob =  new Date(document.getElementById('input-dob').value);
+//    var today = new Date();
+//    console.log(today);
+
+//    if(!dob || isNaN(dob)){
+
+//     alert('plz enter the valid date')
+//     return;
+//    }
+
+//    var year = today.getFullYear()- dob.getFullYear();
+//    var month = today.getMonth() - dob.getMonth();
+//    var day = today.getDate() - dob.getDate();
+
+//    document.getElementById('year').innerText = year;
+//    document.getElementById('month').innerText = month;
+//    document.getElementById('day').innerText = day;
+// });
