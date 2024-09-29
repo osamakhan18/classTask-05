b69c6e3f8683a3c2ef4ad6b78aeb834ce14a7bed
osamakhan18 <badishakhan18@gmail.com>
2024-09-29 00:53:57 -0700

    set the css styling for the inner boxes

--- script.js
+++ script.js
@@ -3,23 +3,23 @@
 
 var btn = document.getElementById('btn')
 
-// btn.addEventListener('click',function(){
+btn.addEventListener('click',function(){
 
-//     var dob =  new Date(document.getElementById('input-dob').value);
-//    var today = new Date();
-//    console.log(today);
+    var dob =  new Date(document.getElementById('input-dob').value);
+   var today = new Date();
+   console.log(today);
 
-//    if(!dob || isNaN(dob)){
+   if(!dob || isNaN(dob)){
 
-//     alert('plz enter the valid date')
-//     return;
-//    }
+    alert('plz enter the valid date')
+    return;
+   }
 
-//    var year = today.getFullYear()- dob.getFullYear();
-//    var month = today.getMonth() - dob.getMonth();
-//    var day = today.getDate() - dob.getDate();
+   var year = today.getFullYear()- dob.getFullYear();
+   var month = today.getMonth() - dob.getMonth();
+   var day = today.getDate() - dob.getDate();
 
-//    document.getElementById('year').innerText = year;
-//    document.getElementById('month').innerText = month;
-//    document.getElementById('day').innerText = day;
-// });
+   document.getElementById('year').innerText = year;
+   document.getElementById('month').innerText = month;
+   document.getElementById('day').innerText = day;
+});
