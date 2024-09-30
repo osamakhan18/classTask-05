7d35d12bef9a4c5a920c50b22942af1ec1e6f75f
osamakhan18 <badishakhan18@gmail.com>
2024-09-30 06:56:16 -0700

    add the media querries

--- script.js
+++ script.js
@@ -19,6 +19,18 @@ btn.addEventListener('click',function(){
    var month = today.getMonth() - dob.getMonth();
    var day = today.getDate() - dob.getDate();
 
+   if (day < 0) {
+    month--;
+    // var lastMonthDays = new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+    day += 30;
+}
+
+
+if (month < 0) {
+    year--;
+    month += 12;
+}
+
    document.getElementById('year').innerText = year;
    document.getElementById('month').innerText = month;
    document.getElementById('day').innerText = day;
